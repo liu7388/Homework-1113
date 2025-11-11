a39222dbe462974d4f3d29484f111625943c93fc
liu7388 <ting20040906@gmail.com>
2025-11-11 20:18:53 +0800

    feat(CH2): Add Sample2_9.java

---
+++ CH2/Sample2_9.java
@@ -0,0 +1,16 @@
+// CH2 P.24
+
+package CH2;
+
+public class Sample2_9
+{
+    public static void main(String[] args) 
+    {
+       for(int i = 1; i <= 5; i++)
+       {
+        System.out.println("第"+ i + "次的迴圈");
+       }
+
+       System.out.println("迴圈結束");
+    }
+}
