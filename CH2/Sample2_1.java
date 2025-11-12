b2066467d0fc971ed683f14c8631d2a040aa3352
liu7388 <ting20040906@gmail.com>
2025-11-12 11:04:32 +0800

    refactor(CH2): Replace BufferedReader with Scanner for simpler input in Sample2_1.java

--- CH2/Sample2_1.java
+++ CH2/Sample2_1.java
@@ -1,7 +1,8 @@
 // CH2 P.7 
-import java.io.BufferedReader;
+// import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+// import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Sample2_1 
 {
@@ -9,10 +10,17 @@ public class Sample2_1
     {
         System.out.println("請輸入整數");
 
+        // 原本程式碼
+        /*
         BufferedReader br = 
             new BufferedReader(new InputStreamReader(System.in));
 
         int num = Integer.parseInt(br.readLine());
+         */
+
+        // 更改為Scanner
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
 
         if(num == 1)
         {
@@ -21,6 +29,7 @@ public class Sample2_1
         }
         
         System.out.println("結束處理");
+        sc.close();
     }
     
 }
